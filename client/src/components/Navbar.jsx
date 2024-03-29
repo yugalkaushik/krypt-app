6ec2ca8b46be0c4c0dce7139a18321e27b056133
Yugal Kaushik <yugalkaushik14@gmail.com>
2024-03-29 23:01:38 +0530

    added changes

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+import logo from '../../Images/logo.png';
+
+const Navbar = () => {
+    return (
+        <h1>Navbar</h1>
+    );
+}
+
+export default Navbar;
