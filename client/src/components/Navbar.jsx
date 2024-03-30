68fcfd6fba1c4b2a5f56e58a3fc55f0d2079a19f
Yugal Kaushik <yugalkaushik14@gmail.com>
2024-03-30 22:37:21 +0530

    added changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,37 @@
-import { HiMenuAlt4 } from 'react-icons/hi';
-import { AiOutlineClose } from 'react-icons/ai';
-import logo from '../../Images/logo.png';
+import { useState } from 'react';
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+import logo from '../../images/logo.png';
 
-const Navbar = () => {
+const NavbarItem = ({ title, classProps }) => {
     return (
-        <h1>Navbar</h1>
+        <li className={`mx-4 cursor-pointer ${classProps}`}>
+            {title}
+        </li>
     );
 }
+
+const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+    return (
+        <nav className='w-full flex md:justify-center'>
+            <div className='md:flex-[0.5] flex-initial justify-center items-center'>
+                <img src={logo} alt="logo" className='w-32 cursor-pointer' />
+            </div>
+            <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
+                {["Market", "Example", "Tutorial", "Wallets"].map((item, index) => (
+                    <NavbarItem key={item + index} title={item} />
+                ))}
+                <li className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]'>Login</li>
+            </ul>
+            <div className='flex-relative'>
+                {toggleMenu
+                    ? <AiOutlineClose fontSize={28} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu(false)} />
+                    : <HiMenuAlt4  fontSize={28} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu(true)} />
+                }
+            </div>
+        </nav>
+    );
+}
+
 export default Navbar;
