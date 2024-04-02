ad294374068dcdf1884840036a772d0a76e69b32
Yugal Kaushik <yugalkaushik14@gmail.com>
2024-04-02 22:47:25 +0530

    added changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 import logo from '../../images/logo.png';
 
+
 const NavbarItem = ({ title, classProps }) => {
     return (
         <li className={`mx-4 cursor-pointer ${classProps}`}>
@@ -10,6 +11,7 @@ const NavbarItem = ({ title, classProps }) => {
         </li>
     );
 }
+
 
 const Navbar = () => {
     const [toggleMenu, setToggleMenu] = useState(false);
