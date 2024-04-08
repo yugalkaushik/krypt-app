0ce31f9f7ee5d4ff0c866532fa8397963646e0a4
Yugal Kaushik <yugalkaushik14@gmail.com>
2024-04-08 21:44:41 +0530

    added changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 import logo from '../../images/logo.png';
 
-
 const NavbarItem = ({ title, classProps }) => {
     return (
         <li className={`mx-4 cursor-pointer ${classProps}`}>
@@ -18,12 +17,14 @@ const Navbar = () => {
             <div className='md:flex-[0.5] flex-initial justify-center items-center'>
                 <img src={logo} alt="logo" className='w-32 cursor-pointer' />
             </div>
+
             <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
                 {["Market", "Example", "Tutorial", "Wallets"].map((item, index) => (
                     <NavbarItem key={item + index} title={item} />
                 ))}
                 <li className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]'>Login</li>
             </ul>
+            
             <div className='flex-relative'>
                 {toggleMenu ? (
                     <AiOutlineClose fontSize={28} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu(false)} />
